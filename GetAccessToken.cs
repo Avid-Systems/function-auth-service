daec61de92a36ee9361199c4f0a4bce68300b880
Aleksei Andreev <nilokillian@gmail.com>
2024-04-23 10:11:10 +1200

    init

---
+++ GetAccessToken.cs
@@ -0,0 +1,54 @@
+using System.Collections.Specialized;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+
+namespace Avid.Function
+{
+    public class GetAccessToken(ILoggerFactory loggerFactory)
+    {
+        private readonly ILogger _logger = loggerFactory.CreateLogger<GetAccessToken>();
+        private readonly string[] scopes = new string[] { $"{Environment.GetEnvironmentVariable("OrgUrl")}/.default" };
+
+        [Function("GetAccessToken")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("HTTP GetToken");
+
+            var config = new NameValueCollection
+                {
+                    { "clientId", Environment.GetEnvironmentVariable("ClientId") },
+                    { "clientSecret", Environment.GetEnvironmentVariable("ClientSecret") },
+                    { "tenantId", Environment.GetEnvironmentVariable("TenantId") }
+                };
+
+
+
+            var app = ConfidentialClientApplicationBuilder.Create(config["clientId"])
+                .WithClientSecret(config["clientSecret"])
+                .WithAuthority(new Uri($"https://login.microsoftonline.com/{config["tenantId"]}"))
+                .Build();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            try
+            {
+                var resultTest = await app.AcquireTokenOnBehalfOf(scopes, new UserAssertion("assertion")).ExecuteAsync();
+                var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+                var tokenResponse = new { token = result.AccessToken };
+
+                await response.WriteAsJsonAsync(tokenResponse);
+                response.StatusCode = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Error acquiring token.");
+                response.StatusCode = HttpStatusCode.InternalServerError;
+            }
+
+            return response;
+        }
+    }
+}
